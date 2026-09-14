d7d466898c768e34749ba7bb80f7bdeb9ae69c26
Katarina Holm <katarinaholm89@gmail.com>
2026-09-14 12:55:41 +0200

    Feat: sciss  layout and functionality to SearchAutoComplete. Not connected to backend yet.

---
+++ src/atoms/searchAutoComplete.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+export default function SearchAutoComplete({value, onChange, results, onSelect}){
+    const [isFocused, setIsFocused] = useState(false);
+
+    function handleBlur(){
+        setTimeout(() => setIsFocused(false), 150);
+        
+    }
+
+  return (
+    <div className="relative w-full max-w-lg">
+      <label className="input w-full">
+        <svg
+          className="h-[1em] opacity-50"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <g
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="2.5"
+            fill="none"
+            stroke="currentColor"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+          </g>
+        </svg>
+        <input 
+         type="search" 
+         className="grow" 
+         placeholder="Sök livsmedel"
+         required
+         value ={value}
+         onChange={onChange}
+         onFocus={() => setIsFocused(true)}
+         onBlur={handleBlur}
+          />
+      </label>
+
+      {isFocused && results.length > 0 &&(
+        <div className="absolute z-50 left-0 right-0 w-auto bg-base-100 shadow rounded-box mt-1 border border-base-300 max-h-60 overflow-y-auto overflow-x-hidden">
+        <ul className="menu bg-transparant p-1">
+            {results.map((foodItem) => (
+                <li key={foodItem.id}>
+                    <button 
+                    className="grid grid-cols-12 items-center w-full gap-2 text-left"
+                    onClick={onSelect}
+                    >
+                      <span className="col-span-7 truncate">{foodItem.name}</span>
+                      <span className="text-xs text-base-content/50  col-span-5 text-right truncate">{foodItem.category}</span>                      
+                    </button>
+                </li>
+            ))}
+        </ul>
+        </div>
+    )}
+    </div>
+  );
+}
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,11 +1,20 @@
+import SearchAutoComplete from "../atoms/searchAutoComplete";
 
-
-export default function HomePage(){
+export default function HomePage(){ 
+    const testArray =  [{ id: 1, name: 'Test 1', category:'Grönsaker, potatis & rotfrukter' }, { id: 1, name: 'Test 1', category:'Grönsaker, potatis & rotfrukter' }, { id: 1, name: 'Test 1', category:'Grönsaker, potatis & rotfrukter' }, { id: 1, name: 'Test 1', category:'Grönsaker, potatis & rotfrukter' },{ id: 1, name: 'Test 1', category:'Grönsaker, potatis & rotfrukter' },{ id: 2, name: 'Test 2 bla bla bla bla bla bla bla bla test test test', category:'Grönsaker, potatis & rotfrukter'}, { id: 3, name: 'Kyckling rå med skinn', category:'Kött, fågel & chark' }, { id: 1, name: 'Majonnäs 80 % fett', category: 'Färdiga rätter, såser & snabbmat'}, { id: 1, name: 'Test 1' }, { id: 1, name: 'Test 1', category: 'Färdiga rätter, såser & snabbmat' }, { id: 1, name: 'Test 1', category: 'Färdiga rätter, såser & snabbmat' }, ]
 
     return(
         <>
-        <h1>Oxalat och näringskollen</h1>
+        <SearchAutoComplete 
+        
+         results = {testArray}
+       
+         />
         <p>Under utveckling...</p>
         </>
     )
 }
+
+//  value=
+        //  onChange= 
+         //  onSelect=
