fd0047e44f2ec15c0d0aa339f70ca4cdef8957f2
Katarina Holm <katarinaholm89@gmail.com>
2026-09-23 20:07:59 +0200

    refactor: change of name to inputLogin.

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,4 +1,4 @@
-import InputField from "../atoms/input";
+import InputLogin from "../atoms/inputLogin";
 import { useState } from "react";
 import { login } from "../services/AuthService";
 import { useNavigate } from "react-router";
@@ -21,8 +21,8 @@ export default function Login(){
         <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
             <legend className="fieldset-legend">Logga in</legend>
             <form onSubmit={handleLogin}>
-                <InputField type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} name="email" required />
-                <InputField type="password" placeholder="Lösenord" value={password} onChange={(e) => setPassword(e.target.value)} name="password" required />   
+                <InputLogin type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} name="email" required />
+                <InputLogin type="password" placeholder="Lösenord" value={password} onChange={(e) => setPassword(e.target.value)} name="password" required />   
                 <ButtonSecondary text="Logga in" type="submit" />                
             </form>
         </fieldset>
